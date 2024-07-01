3e17602e472fccb29426a8c159c03635421da52c
Amanat1599 <154678817+Amanat1599@users.noreply.github.com>
2024-07-01 23:13:55 +0500

    class_Update

--- src/MLProject/components/data_ingestion.py
+++ src/MLProject/components/data_ingestion.py
@@ -4,3 +4,11 @@ from src.MLProject.exception import CustomException
 from src.MLProject.logger import logging
 import numpy as np
 import pandas as pd
+
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
